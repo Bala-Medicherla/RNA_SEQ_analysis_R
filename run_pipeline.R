2c504a4bd0150f18c05dbf868443af67bdbcdd88
Sriram Medicherla <srirammedicherla@Srirams-MacBook-Air.local>
2026-01-13 23:24:19 -0500

    added new source scripts

--- run_pipeline.R
+++ run_pipeline.R
@@ -4,5 +4,8 @@ source("scripts/00_prepare_tcga_data.R")
 source("scripts/01_qc_and_transform.R")
 source("scripts/02_deseq2_analysis.R")
 source("scripts/03_visualizations.R")
+source("scripts/install_dependencies.R")
+source("scripts/04_gsea_analysis.R")
+
 
 message("Pipeline completed. Check the figures/ and results/ folders.")
