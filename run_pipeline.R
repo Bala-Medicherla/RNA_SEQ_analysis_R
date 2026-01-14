201c2db3df8d2ab14d54e565e7ca374d378f270d
Sriram Medicherla <srirammedicherla@Mac.home>
2026-01-14 09:58:25 -0500

    added a new script

--- run_pipeline.R
+++ run_pipeline.R
@@ -1,11 +1,11 @@
 message("Running TCGA-BRCA RNA-seq pipeline...")
 
+source("scripts/install_dependencies.R")
 source("scripts/00_prepare_tcga_data.R")
 source("scripts/01_qc_and_transform.R")
 source("scripts/02_deseq2_analysis.R")
 source("scripts/03_visualizations.R")
-source("scripts/install_dependencies.R")
 source("scripts/04_gsea_analysis.R")
-
+source("scripts/05_session_info.R")
 
 message("Pipeline completed. Check the figures/ and results/ folders.")
